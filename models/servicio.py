2315b78a845fe8361e37bd9bfb17c53ac993d23f
MaritzaLavado <maritzalavado122018@gmail.com>
2026-05-07 07:39:37 -0500

    Agregar comenatarios guía

--- models/servicio.py
+++ models/servicio.py
@@ -2,17 +2,17 @@ from abc import ABC, abstractmethod
 
 class Servicio(ABC):
     def __init__(self, nombre, tarifa, horas):
-        self.__nombre = nombre
-        self.__tarifa = tarifa  # Este valor vendrá predefinido por las hijas
-        self.__horas = horas
+        self.__nombre = nombre  # Este nombre sera predefinido por las hijas
+        self.__tarifa = tarifa  # Este valor será predefinido por las hijas
+        self.__horas = horas    # Las define el usuario, pedirlas en el main
 
-    @abstractmethod
+    @abstractmethod  # Es un método abstracto
     def calcularCosto(self, iva=0.19, descuento=None):
         #Cada Hija debe de llevar su cálculo especifico usando los atributos internos
         pass
 
     def descripcion(self):
-        return f"El servicio que ha seleccionado es: {self.get_nombre()}"
+        return f"El servicio que ha seleccionado es: {self.get_nombre()}" # Mensaje base para cada hija
     
     #Getters para obtener datos privados
     def get_nombre(self):
