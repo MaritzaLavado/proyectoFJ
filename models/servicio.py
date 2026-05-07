409f028028b59cd29b209734ac22bb2206385924
MaritzaLavado <maritzalavado122018@gmail.com>
2026-05-07 07:07:02 -0500

    Implementar lógica base de servicio

--- models/servicio.py
+++ models/servicio.py
@@ -1,13 +1,25 @@
 from abc import ABC, abstractmethod
 
 class Servicio(ABC):
-    def __init__(self, nombre=""):
-        self._nombre = nombre
+    def __init__(self, nombre, tarifa, horas):
+        self.__nombre = nombre
+        self.__tarifa = tarifa  # Este valor vendrá predefinido por las hijas
+        self.__horas = horas
 
     @abstractmethod
-    def calcular_costo(self, *args, **kwargs):
-        return 0
+    def calcularCosto(self, iva=0.19, descuento=None):
+        #Cada Hija debe de llevar su cálculo especifico usando los atributos internos
+        pass
 
-    @abstractmethod
     def descripcion(self):
-        return "Servicio base"
+        return f"El servicio que ha seleccionado es: {self.get_nombre()}"
+    
+    #Getters para obtener datos privados
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_tarifa(self):
+        return self.__tarifa
+
+    def get_horas(self):
+        return self.__horas
